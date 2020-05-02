6897d31f72de65fc82dc59217dc641a1462b11c8
Joep Sales <sales.joep@gmail.com>
2020-05-02 14:42:10 +0200

    Added agenda Dto and Interface

---
+++ Cinemaat_layers.INTERFACES/IAgenda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinemaat_layers.INTERFACES
+{
+    public interface IAgenda
+    {
+        int MovieId { get; set; }
+        int MovieHallId { get; set; }
+        bool IsValid { get; set; }
+        DateTime Time { get; set; }
+    }
+}
