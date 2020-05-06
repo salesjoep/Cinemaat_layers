b4e6c866805a3f13830a38916e1a6864ad589a5f
Joep Sales <sales.joep@gmail.com>
2020-05-06 12:35:34 +0200

    -Updated views

---
+++ Cinemaat_layers.DAL/IMovieRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cinemaat_layers.INTERFACES;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinemaat_layers.DAL
+{
+    public interface IMovieRepository
+    {
+        void Add(IMovie movie);
+        IEnumerable<IMovie> GetAll();
+        IEnumerable<IMovie> Select1917Movie();
+
+        IMovie Update(IMovie movieChanges);
+        IMovie Delete(string movieName);
+    }
+}
--- Cinemaat_layers.DAL/MovieRepository.cs
+++ Cinemaat_layers.DAL/MovieRepository.cs
@@ -1,24 +1,39 @@
 ﻿using Cinemaat_layers.INTERFACES;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Cinemaat_layers.DAL
 {
-    public class MovieRepository
+    public class MovieRepository : IMovieRepository
     {
         private readonly IMovieContext _context;
+        private List<IMovie> movieList;
 
         public MovieRepository(IMovieContext context)
         {
             _context = context;
         }
-
+        //CREATE - RUD
         public void Add(IMovie movie)
         {
             _context.Add(movie);
         }
+        //CRU - DELETE
+        public IMovie Delete(string movieName)
+        {
+            IMovie movie = movieList.FirstOrDefault(e => e.MovieName == movieName);
+            if (movie != null)
+            {
+                movieList.Remove(movie);
+            }
+            return movie;
 
+
+        }
+
+        //C - READ - UD
         public IEnumerable<IMovie> GetAll()
         {
             return _context.GetAll();
@@ -27,6 +42,22 @@ namespace Cinemaat_layers.DAL
         public IEnumerable<IMovie> Select1917Movie()
         {
             return _context.Select1917Movie();
+        }
+
+        //CR - UPDATE - D
+        public IMovie Update(IMovie movieChanges)
+        {
+            IMovie movie = movieList.FirstOrDefault(e => e == movieChanges);
+            if (movie != null)
+            {
+                movie.MovieName = movieChanges.MovieName;
+                movie.Description = movieChanges.Description;
+                movie.DateCreated = movieChanges.DateCreated;
+                movie.Genre = movieChanges.Genre;
+                movie.Review = movieChanges.Review;
+                movie.Rating = movieChanges.Rating;
+            }
+            return movie;
         }
     }
 }
