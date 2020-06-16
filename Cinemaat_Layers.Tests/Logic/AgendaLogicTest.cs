76145fc08bb99f553b21ac9a3ff540c05e62c429
Joep Sales <sales.joep@gmail.com>
2020-06-16 16:09:50 +0200

    -Started on logic tests

---
+++ Cinemaat_Layers.Tests/Logic/AgendaLogicTest.cs
@@ -0,0 +1,32 @@
+﻿using Cinemaat_layers.DAL;
+using Cinemaat_layers.INTERFACES.Logic;
+using Cinemaat_layers.LOGIC;
+using Cinemaat_Layers.Tests.MockData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinemaat_Layers.Tests.Logic
+{
+    
+    public class AgendaLogicTest
+    {
+        public static IAgendaContext context = new AgendaContext(MockConnection.connection);
+        public IAgendaLogic sut = new AgendaLogic(context);
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAllAgendasTest()
+        {
+            var result = sut.GetAllAgendas(1);
+            Assert.IsNotNull(result);
+
+            MockConnection.CloseConnection();
+        }
+    }
+}
---
+++ Cinemaat_Layers.Tests/Logic/FavouriteLogicTest.cs
@@ -0,0 +1,33 @@
+﻿using Cinemaat_layers.DAL.Context_Classes;
+using Cinemaat_layers.INTERFACES.Context;
+using Cinemaat_layers.INTERFACES.Logic;
+using Cinemaat_layers.LOGIC;
+using Cinemaat_Layers.Tests.MockData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinemaat_Layers.Tests.Logic
+{
+
+    public class FavouriteLogicTest
+    {
+        public static IFavouriteContext context = new FavouriteContext(MockConnection.connection);
+        public IFavouriteLogic sut = new FavouriteLogic(context);
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void GetAllfavourites()
+        {
+            var result = sut.GetAll(12);
+            Assert.IsNotNull(result);
+
+            MockConnection.CloseConnection();
+        }
+    }
+}
